eca61ed632b4924eaae21aa0d18a5f43448cff6f
Elliotbillioncodes <elliotacolatse@gmail.com>
2026-06-01 12:37:37 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Elliot Elikplim Acolatse
+# STUDENT ID: 2425402788
+# Email: 2425402788@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Elliot Elikplim Acolatse, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    # Print my initials in large stylized letters
+    print()
+    print("...EEA")
+    print()
+    print("...EEEEEEEEEEEE    ...EEEEEEEEEEEE    ...    AAAA        ...")
+    print("...EE              ...EE              ...   AAAAAA       ...")
+    print("...EE              ...EE              ...  AA    AA      ...")
+    print("...EEEEEEEE        ...EEEEEEEE        ... AA      AA     ...")
+    print("...EEEEEEEE        ...EEEEEEEE        ...AAAAAAAAAAAA    ...")
+    print("...EE              ...EE              ...AAAAAAAAAAAA    ...")
+    print("...EE              ...EE              ...AA        AA    ...")
+    print("...EE              ...EE              ...AA        AA    ...")
+    print("...EEEEEEEEEEEE****...EEEEEEEEEEEE****...AA        AA****...")
+    print("...EEEEEEEEEEEE****...EEEEEEEEEEEE****...AA        AA****...")
+    print()
 
 
 main()
